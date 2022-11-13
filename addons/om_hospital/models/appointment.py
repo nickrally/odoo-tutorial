e094b5367ee002bceb2b9a2ee2d4209b4f3d0355
Nik Musaelian <nik@ursamajor.com>
2022-11-13 00:21:02 -0700

    19-26

--- addons/om_hospital/models/appointment.py
+++ addons/om_hospital/models/appointment.py
@@ -1,8 +1,28 @@
+import time
 from odoo import api, models, fields
 
 class HospitalAppointment(models.Model):
     _name = "hospital.appointment"
     _inherit=['mail.thread','mail.activity.mixin']
     _description = "Hospital Appointment"
+    _rec_name = 'apt_ref'
 
     patient_id = fields.Many2one('hospital.patient', string='Patient')
+    gender = fields.Selection(related='patient_id.gender')
+    ref = fields.Char(related='patient_id.ref', readonly=False)
+    appointment_time = fields.Datetime(string='Appointment Time', default=fields.Datetime.now)
+    booking_date = fields.Date(string='Booking Date', default=fields.Date.context_today)
+    country = fields.Char(string="Country")
+    apt_ref = fields.Char(string="Apt Ref", compute="_compute_apt_ref", store=True)
+
+    @api.onchange('patient_id')
+    def onchage_patient_id(self):
+        self.country = self.patient_id.country
+
+    @api.depends('ref','appointment_time')
+    def _compute_apt_ref(self):
+        for record in self:
+            if record.ref and record.appointment_time:
+                record.apt_ref  =  record.ref + str(int(time.mktime(record.appointment_time.timetuple())))
+            else:
+                record.apt_ref = '0'
--- addons/om_hospital/models/patient.py
+++ addons/om_hospital/models/patient.py
@@ -1,12 +1,25 @@
+from datetime import date
 from odoo import api, models, fields
 
 class HospitalPatient(models.Model):
     _name = "hospital.patient"
     _inherit=['mail.thread','mail.activity.mixin']
     _description = "Hospital Patient"
+    _rec_name = 'name'
 
     name = fields.Char(string="Name", tracking=True)
     ref = fields.Char(string="Reference")
-    age = fields.Integer(string="Age", tracking=True)
+    date_of_birth = fields.Date(string="Date of Birth")
+    age = fields.Integer(string="Age", compute="_compute_age", tracking=True)
     gender = fields.Selection([('male','Male'),('female','Female')], string="Gender")
     active = fields.Boolean(string="Active", default=True)
+    country = fields.Char(string="Country")
+
+    @api.depends('date_of_birth')
+    def _compute_age(self):
+        for record in self:
+            today = date.today()
+            if record.date_of_birth:
+                record.age  = today.year - record.date_of_birth.year
+            else:
+                record.age = 0
