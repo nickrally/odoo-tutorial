448d64a14b30a13136a22fc7ebff403e06690943
Nik Musaelian <nik@ursamajor.com>
2022-11-23 23:47:35 -0700

    73

--- addons/om_hospital/models/patient.py
+++ addons/om_hospital/models/patient.py
@@ -22,6 +22,11 @@ class HospitalPatient(models.Model):
         vals['ref'] = self.env['ir.sequence'].next_by_code('hospital.patient')
         return super(HospitalPatient, self).create(vals)
 
+    def write(self, vals):
+        if not self.ref and not vals.get('ref'):
+            vals['ref'] = self.env['ir.sequence'].next_by_code('hospital.patient')
+        return super(HospitalPatient, self).write(vals)
+
     @api.depends('date_of_birth')
     def _compute_age(self):
         for record in self:
